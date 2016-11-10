e5cc7d7b6cc246bd0129bbe08a1ec15f66ffa657
soe-bduke@uoguelph.ca <soe-bduke@WS5.mlrg.soe.uoguelph.ca>
2016-11-10 09:28:29 -0500

    More intro

--- appetite_whetting.py
+++ appetite_whetting.py
@@ -1,2 +1,21 @@
+"""
+Generates some data in two dimensions, then fits it to a line.
+"""
 import tensorflow as tf
 import numpy as np
+
+def whet_appetite():
+    """
+    Whets appetite.
+    """
+    x_data = np.random.rand(100).astype(np.float32)
+    y_data = x_data * 0.1 + 0.3
+
+    W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+    b = tf.Variable(tf.zeros([1]))
+    y = W * x_data + b
+
+    loss = tf.reduce_mean(tf.square(y - y_data))
+
+if __name__ == '__main__':
+    whet_appetite()
